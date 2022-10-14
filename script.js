227d29f22aa60226f7b7a292b81258c31e537ec9
btrzdev <btrz.dev@gmail.com>
2022-10-14 20:58:00 -0300

    feat: consultCEP API

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function hamburguerMenu(){
+const hamburguerMenu = () => {
     let navBarMenu = document.getElementById("navBarMenuId"); 
     navBarMenu.classList.add("navBarMenu")
-    console.log(navBarMenu.classList)
 
     if (navBarMenu.classList[0]=== "navBarMenu") {
         navBarMenu.classList.remove("navBarMenu")
@@ -9,7 +8,30 @@ function hamburguerMenu(){
     } else {
         navBarMenu.classList.add("navBarMenu")
         navBarMenu.classList.remove("navBarMenuDeactive")
-
-
     }
 }
+
+
+const consultCEP = () => {  
+    const cepInput = document.getElementById("cepID").value
+    let addressInput = document.getElementById("addressID").value
+    let cityInput = document.getElementById("cityID").value
+    let stateInput = document.getElementById("stateID").value
+
+    console.log(cepInput)
+    fetch(`https://viacep.com.br/ws/${cepInput}/json/`, {headers: {}})
+    .then(response => response.json())
+    .then(data =>(    
+          
+         addressInput = `${data.logradouro} - ${data.bairro}`,
+         cityInput = `${data.localidade}`,
+         stateInput = `${data.uf}`         
+
+        )                      
+    ).catch((err) => console.log(err))
+
+    
+    
+}
+
+
